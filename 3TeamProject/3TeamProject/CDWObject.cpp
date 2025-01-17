3e2afb58b0a33c7ae885c55639e37c1fcb84c06d
DongWan-Hong <thsu5957@naver.com>
2025-01-17 21:37:22 +0900

    1월17일 동완(최종)

--- 3TeamProject/3TeamProject/CDWObject.cpp
+++ 3TeamProject/3TeamProject/CDWObject.cpp
@@ -2,25 +2,37 @@
 #include "CDWObject.h"
 
 CDWObject::CDWObject()
+    :m_pRenderPoint(nullptr)
 {
-
-	ZeroMemory(&m_tInfo, sizeof(m_tInfo));
-	D3DXMatrixIdentity(&m_tInfo.matWorld);
+    ZeroMemory(&m_tInfo, sizeof(m_tInfo));
+    D3DXMatrixIdentity(&m_tInfo.matWorld);
 }
 
 CDWObject::~CDWObject()
 {
+    // 동적 메모리 해제
+    if (m_pRenderPoint)
+    {
+        delete[] m_pRenderPoint;
+        m_pRenderPoint = nullptr;
+    }
 }
 
 void CDWObject::Initialize_OriginPoint(const int& _iCount, const int& _Radius, const float& _fStartAngle)
 {
+    m_vOriginPointvec.clear();
+    m_vPointvec.clear();
     m_vOriginPointvec.reserve(_iCount);
     m_vPointvec.reserve(_iCount);
     m_fRadius = _Radius;
 
+    
+
+
+    m_pRenderPoint = new POINT[_iCount];
+
     float fUnitAngle = 360.f / (float)_iCount;
     float CurAngle(0.f);
-    m_pRenderPoint = new POINT[_iCount];
 
     for (int i = 0; i < _iCount; ++i)
     {
